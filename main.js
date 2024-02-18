e2b0aa84ab1bcec1547eb825487e342406e359e3
ketig <41895005+ketig@users.noreply.github.com>
2024-02-18 16:56:42 +0400

    Update button styles and add a clear button. (generated by blackbox.ai )

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
-function addToDisplay(value) {
-    document.getElementById("display").value += value;
+function addToDisplay(x) {
+    document.getElementById("display").value += x;
 }
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function leftDelete() {
+    // "78+545-34"
+    let str = document.getElementById("display").value;
+    let result = str.substring(0, str.length - 1);// "78+545-3"
+
+    document.getElementById("display").value = result;
+}
+
+function calculate() {
+    let str = document.getElementById("display").value;
+    console.log(str);
+    let result = 0;
+
+    document.getElementById("displayResult").value = result;
+}
+// Math.max(3, 56, 2) // პარამეტრებიანი
+// Math.random() // უპარამეტრებო
