be36eee0f72d866f7c332db080a1b999e60080ea
ketig <41895005+ketig@users.noreply.github.com>
2024-02-25 16:57:26 +0400

    functions

--- main.js
+++ main.js
@@ -1,25 +1,40 @@
 function addToDisplay(x) {
     document.getElementById("display").value += x;
 }
-
 function clearDisplay() {
-    document.getElementById("display").value = "";
+    clearInput("display");
+}
+function clearAll() {
+    // display displayResult
+    clearDisplay();
+    clearInput("displayResult");
+}
+function clearInput(id) {
+    document.getElementById(id).value = "";
 }
 
 function leftDelete() {
-    // "78+545-34"
     let str = document.getElementById("display").value;
-    let result = str.substring(0, str.length - 1);// "78+545-3"
+    let result = str.substring(0, str.length - 1);
 
     document.getElementById("display").value = result;
 }
 
 function calculate() {
     let str = document.getElementById("display").value;
-    console.log(str);
-    let result = 0;
+    let result = eval(str);
 
     document.getElementById("displayResult").value = result;
 }
-// Math.max(3, 56, 2) // პარამეტრებიანი
-// Math.random() // უპარამეტრებო
+
+function calculateSin() {
+    let str = document.getElementById("display").value;
+    let result = Math.sin(str);
+    document.getElementById("displayResult").value = result;
+}
+
+// calculateCos()
+// calculateTan()
+// calculateFact()
+// calculateSqrt()
+// https://github.com/ketig/Calculator_IIgr
